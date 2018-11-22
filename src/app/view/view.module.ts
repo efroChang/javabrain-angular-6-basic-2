72b796b3d8cd1b629f06502703428a9aa837fc25
Eric Chang <efrochang@gmail.com>
2018-11-22 16:13:13 -0600

    Multile Modules: ng generate module ...

---
+++ src/app/view/view.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ViewComponent } from './view/view.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    ViewComponent
+  ],
+  exports: [
+    ViewComponent
+  ]
+})
+export class ViewModule { }
