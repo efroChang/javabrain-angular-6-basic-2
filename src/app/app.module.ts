13c8862c2e4f0b838122ca716e3894deed55daee
Eric Chang <efrochang@gmail.com>
2018-11-23 13:33:32 -0600

    Http Client: Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ViewModule } from './view/view.module';
 
@@ -10,7 +10,8 @@ import { ViewModule } from './view/view.module';
   ],
   imports: [
     BrowserModule,
-    ViewModule
+    ViewModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
